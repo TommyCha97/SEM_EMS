9ebe9716efd9337c97e4c61d2f570087b576cdd4
TommyCha97 <TommyCha11197@gmail.com>
2020-12-14 16:33:36 +0800

    First Push

---
+++ ApplicationLayer/UserView/sUpdateEventPackage.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>EMS Supplier Menu</title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="../../libs/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="../../libs/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="../../libs/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../../libs/css/sb-admin.css" rel="stylesheet">
+
+  </head>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+
+  .form-container .btn:hover, .open-button:hover {
+    opacity: 1;
+  }
+
+  body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+/* Full-width input fields */
+input[type=text] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* Add a background color when the inputs get focus */
+input[type=text]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity:1;
+}
+
+.signupbtn {
+  float: left;
+  width: 50%;
+}
+
+/* Add padding to container elements */
+.container {
+  padding: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: #474e5d;
+  padding-top: 50px;
+}
+
+textarea {
+  width: 100%;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  font-size: 16px;
+  resize: none;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* Style the horizontal ruler */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+.close:hover,
+.close:focus {
+  color: #f44336;
+  cursor: pointer;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 500px) {
+  .cancelbtn, .signupbtn {
+     width: 100%;
+  }
+}
+  </style>
+<body id="page-top">
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1" href="supplierMenu.php?id=S_1">Event Management System</a>
+
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Navbar Search -->
+     
+      
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+       
+         
+      </form>
+
+<div>
+</div>       <!-- Navbar -->
+      <ul class="navbar-nav ml-auto ml-md-0">
+            <?php
+if(isset($_GET["id"])){
+
+$id1=$_GET['id'];
+
+
+
+}
+      ?>
+         
+        </li>
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle fa-fw"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+          </div>
+        </li>
+      </ul>
+
+    </nav>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="supplierMenu.php?id=S_1">
+            <i class="fas fa-home"></i>
+            <span>Home</span>
+          </a>
+        </li>
+        <!-- Sidebar for supplier-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-shuttle-van"></i>
+            <span>Supplier</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Login Screens:</h6>
+            <a class="dropdown-item active" href="#"  >Log out</a>
+            <a class="dropdown-item" href="#" ></a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Equipment Pages:</h6>
+               <!--Equipment Page -->
+            <a class="dropdown-item" href="#" >Equipment List</a>
+            <a class="dropdown-item" href="#" >Add Equipment</a>
+            <a class="dropdown-item" href="#" >Update Equipment</a>
+            <a class="dropdown-item " href="#" >Delete Equipment</a>
+               <div class="dropdown-divider"></div>
+               <!--Other Relates Page -->
+              <h6 class="dropdown-header">Other Pages:</h6>
+             
+              <?php
+              echo"<a class='dropdown-item' href='sStatus.php?id1=".$id1." '>Status</a>";
+
+              ?>
+          </div>
+        </li>
+          <!-- Sidebar for Customer-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-shopping-bag"></i>
+            <span>Customer</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Login Screens:</h6>
+            <a class="dropdown-item active" href="#" onclick="alertLogin()">Login</a>
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Register</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Event and Equipment:</h6>
+               <!--Equipment Page -->
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Event Package</a>
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Equipment Rental</a>
+               <!--Payment Page -->
+               <div class="dropdown-divider"></div>
+              <h6 class="dropdown-header" >Payment pages:</h6>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Pay</a>
+
+               <!--Other Relates Page for customer -->
+               <div class="dropdown-divider"></div>
+              <h6 class="dropdown-header">Other pages:</h6>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Rating</a>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Question & Answer</a>
+          </div>
+        </li>
+
+           <!-- Sidebar for Event Organizer-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-calendar-alt"></i>
+            <span>Event Organizer</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Login Screens:</h6>
+            <a class="dropdown-item active" href="#" onclick="alertLogin()">Login</a>
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Register</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Event and Equipment:</h6>
+               <!--Equipment Page -->
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Event Package</a>
+            <a class="dropdown-item" href="#" onclick="alertLogin()">Equipment Rental</a>
+               <!--Payment Page -->
+               <div class="dropdown-divider"></div>
+              <h6 class="dropdown-header">Payment pages:</h6>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Pay</a>
+
+               <!--Other Relates Page for event organizer -->
+               <div class="dropdown-divider"></div>
+              <h6 class="dropdown-header">Other pages:</h6>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Rating</a>
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Question & Answer</a>
+          </div>
+        </li>
+
+
+           <!-- Sidebar for administator-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           <i class="fas fa-user-cog"></i>
+            <span>Admin</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Logout Screens:</h6>
+            <a class="dropdown-item active" href="#" onclick="alertLogin()" >Log in</a>
+
+               <!--Approve membership Page -->
+           <div class="dropdown-divider"></div>
+              <h6 class="dropdown-header">Approve pages:</h6>
+           
+              <a class="dropdown-item" href="#" onclick="alertLogin()">Approve Member</a>
+          </div>
+        </li>
+
+      </ul>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="supplierMenu.php?id=S_1">Home</a>
+            </li>
+            <li class="breadcrumb-item active">Event Package</li>
+          </ol>
+
+          <!-- Page Content -->
+          <h1>Event Package</h1>
+          <hr>
+
+<!-- Top menu on small screens -->
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()"></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:150px;margin-right:200px">
+
+  <form class="modal-content" action="sViewEventPackage.php?id=S_1" method="post">
+    <div class="container">
+      <h1><b>Edit Event Package</b></h1>
+      <br><label for="title"><b>Title</b></label>
+      <input type="text" placeholder="Enter the title" name="title" required>
+
+      <label for="details"><b>Details</b></label><br>
+      <textarea placeholder="The details.." name="details" required></textarea>
+
+      Upload Image: <input type="file" name="pic" id="pic"><br>
+
+      <div class="clearfix">
+        <button type="submit" class="signupbtn" onclick="sendMessage()">Send</button>
+      </div>
+    </div>
+  </form>
+</div>
+<br>
+ <footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © SDW GROUP 4 EVENT MANAGEMENT SYSTEM 2018/2019</span>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
+ <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="index.php">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</body>
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+           <!-- Bootstrap core JavaScript-->
+    <script src="../../libs/vendor/jquery/jquery.min.js"></script>
+    <script src="../../libs/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../../libs/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../../libs/js/sb-admin.min.js"></script>
+             </body>
+<script type="text/javascript">
+<script>
+  function sendMessage() {
+    alert("Your package has been updated ");
+  }
+</script>
+<script type="text/javascript">
+ 
+   function alertLogin() {
+  alert("Please Log out your  account First");
+}
+</script>
+</html>
